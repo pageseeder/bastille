5b39d971ab573005dc056e357ff1015df829ae61
clauret <devnull@localhost>
2011-04-13 12:54:10 +1000

    Support direct transformations of PageSeeder content using XSLT and created the PSResource object

--- src/com/weborganic/bastille/pageseeder/PSConnector.java
+++ src/com/weborganic/bastille/pageseeder/PSConnector.java
@@ -3,24 +3,22 @@ package com.weborganic.bastille.pageseeder;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Map.Entry;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.weborganic.berlioz.GlobalSettings;
 import org.weborganic.berlioz.xml.XMLCopy;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -38,7 +36,7 @@ import com.weborganic.bastille.security.ps.PageSeederUser;
  * that needs login access.
  * 
  * @author Christophe Lauret
- * @version 11 April 2011
+ * @version 13 April 2011
  */
 public final class PSConnector {
 
@@ -55,17 +53,7 @@ public final class PSConnector {
   /**
    * The type of resource accessed.
    */
-  private final PSResourceType _type;
-
-  /**
-   * The resource to access.
-   */
-  private final String _resource;
-
-  /**
-   * The parameters to send.
-   */
-  private final Map<String, String> _parameters = new HashMap<String, String>();
+  private final PSResource.Builder _resource;
 
   /**
    * If specified, the request will be made on behalf of that user.
@@ -79,8 +67,7 @@ public final class PSConnector {
    * @param resource The 
    */
   public PSConnector(PSResourceType type, String resource) {
-    this._type = type;
-    this._resource = resource;
+    this._resource = new PSResource.Builder(type, resource);
   }
 
   /**
@@ -98,69 +85,7 @@ public final class PSConnector {
    * @param value The value of the parameter
    */
   public void addParameter(String name, String value) {
-    this._parameters.put(name, value);
-  }
-
-  /**
-   * Returns the URL for this connection.
-   * 
-   * @return the URL for this connection.
-   * 
-   * @throws MalformedURLException If the URL is not well-formed
-   */
-  public URL toURL() throws MalformedURLException {
-    Properties pageseeder = GlobalSettings.getNode("bastille.pageseeder");
-    // Start building the URL
-    StringBuffer url = new StringBuffer();
-    url.append(pageseeder.getProperty("scheme", "http")).append("://");
-    url.append(pageseeder.getProperty("host",   "localhost")).append(":");
-    url.append(pageseeder.getProperty("port",   "8080"));
-
-    // Decompose the resource (in case it contains a query or fragment part)
-    String path  = getURLPath(this._resource);
-    String query = getURLQuery(this._resource);
-    String frag  = getURLFragment(this._resource);
-
-    // Servlets
-    if (this._type == PSResourceType.SERVLET) {
-      url.append(pageseeder.getProperty("servletprefix", "/ps/servlet")).append('/');
-      url.append(path);
-
-    // Services
-    } else if (this._type == PSResourceType.SERVICE) {
-      url.append(pageseeder.getProperty("siteprefix", "/ps"));
-      url.append("/service");
-      url.append(path);
-
-    // Any other resource
-    } else {
-      url.append(path);
-    }
-    // If the session ID is available
-    if (this._user != null && this._user.getJSessionId() != null) {
-      url.append(";jsessionid=").append(this._user.getJSessionId());
-    }
-    // Query Part
-    if (query != null) {
-      url.append(query);
-      url.append("&xformat=xml");
-    } else {
-      url.append("?xformat=xml");
-    }
-    try {
-      for (Entry<String, String> p : this._parameters.entrySet()) {
-        URLEncoder.encode(p.getKey(), "utf-8");
-        url.append("&").append(URLEncoder.encode(p.getKey(), "utf-8"));
-        url.append("=").append(URLEncoder.encode(p.getValue(), "utf-8"));
-      }
-    } catch (UnsupportedEncodingException ex) {
-      // Should never happen as UTF-8 is supported
-      ex.printStackTrace();
-    }
-    // Fragment if any
-    if (frag != null)
-      url.append(frag);
-    return new URL(url.toString());
+    this._resource.addParameter(name, value);
   }
 
   /**
@@ -168,13 +93,13 @@ public final class PSConnector {
    * 
    * @param xml the XML to copy from PageSeeder
    * 
-   * @throws IOException ???
+   * @throws IOException If an error occurs when trying to write the XML.
    * 
    * @return <code>true</code> if the request was processed without errors;
    *         <code>false</code> otherwise.
    */
   public boolean get(XMLWriter xml) throws IOException {
-    return get(xml, null);
+    return get(xml, null, null);
   }
 
   /**
@@ -191,17 +116,57 @@ public final class PSConnector {
    *         <code>false</code> otherwise.
    */
   public boolean get(XMLWriter xml, PSHandler handler) throws IOException {
+    return get(xml, handler, null);
+  }
+
+  /**
+   * Connect to PageSeeder and fetch the XML using the GET method. 
+   * 
+   * <p>Templates can be specified to transform the XML. 
+   * 
+   * @param xml       The XML to copy from PageSeeder
+   * @param templates A set of templates to process the XML (optional)
+   * 
+   * @throws IOException If an error occurs when trying to write the XML.
+   * 
+   * @return <code>true</code> if the request was processed without errors;
+   *         <code>false</code> otherwise.
+   */
+  public boolean get(XMLWriter xml, Templates templates) throws IOException {
+    return get(xml, null, templates);
+  }
+
+  /**
+   * Connect to PageSeeder and fetch the XML using the GET method. 
+   * 
+   * <p>If the handler is not specified, the xml writer receives a copy of the PageSeeder XML.
+   * 
+   * <p>If templates are specified they take precedence over the handler.
+   * 
+   * @param xml       the XML to copy from PageSeeder
+   * @param handler   the handler for the XML (can be used to rewrite the XML)
+   * @param templates A set of templates to process the XML (optional)
+   * 
+   * @throws IOException If an error occurs when trying to write the XML.
+   * 
+   * @return <code>true</code> if the request was processed without errors;
+   *         <code>false</code> otherwise.
+   */
+  private boolean get(XMLWriter xml, PSHandler handler, Templates templates) throws IOException {
+
+    // Build the resource
+    PSResource r = this._resource.build();
 
     // Let's start
-    xml.openElement("ps-"+this._type.toString().toLowerCase(), true);
-    xml.attribute("resource", this._resource);
+    xml.openElement("ps-"+r.type().toString().toLowerCase(), true);
+    xml.attribute("resource", r.name());
 
     boolean ok = true;
 
     // Get the URL
     URL url = null;
     try {
-      url = toURL();
+      url = r.toURL(this._user);
       LOGGER.debug("PageSeeder URL: {}", url.toString());
     } catch (MalformedURLException ex) {
       LOGGER.warn("Malformed URL: {}", ex.getMessage());
@@ -237,7 +202,11 @@ public final class PSConnector {
         // Return content is XML try to parse it
         if (isXML(contentType)) {
           xml.attribute("content-type", "application/xml");
-          ok = parseXML(connection, xml, handler);
+          if (templates != null) {
+            ok = parseXML(connection, xml, templates);
+          } else {
+            ok = parseXML(connection, xml, handler);
+          }
 
         // Text content
         } else if (contentType != null && contentType.startsWith("text/")) {
@@ -357,6 +326,61 @@ public final class PSConnector {
   }
 
   /**
+   * Parse the Response as XML.
+   * 
+   * @param connection The HTTP URL connection.
+   * @param xml        Where the final XML goes.
+   * @param templates  To transform the XML.
+   * 
+   * @return <code>true</code> if the data was parsed without error;
+   *         <code>false</code> otherwise.
+   * 
+   * @throws IOException If an error occurs while writing the XML.
+   */
+  private static boolean parseXML(HttpURLConnection connection, XMLWriter xml, Templates templates) throws IOException {
+    boolean ok = true;
+
+    // Create an XML Buffer
+    StringWriter buffer = new StringWriter();
+
+    InputStream in = null;
+    try {
+      in = connection.getInputStream();
+
+      // Setup the source
+      StreamSource source = new StreamSource(in);
+      source.setSystemId(connection.getURL().toString());
+
+      // Setup the result
+      StreamResult result = new StreamResult(buffer);
+
+      // Create a transformer from the templates
+      Transformer transformer = templates.newTransformer();
+
+      // Process, write directly to the result
+      transformer.transform(source, result);
+
+    } catch (TransformerException ex) {
+      LOGGER.warn("Error while transforming XML data from URL", ex);
+      error(xml, "transform-error", ex.getLocalizedMessage());
+      ok = false;
+
+    } catch (IOException ex) {
+      LOGGER.warn("Error while parsing XML data from URL", ex);
+      error(xml, "io-error", ex.getLocalizedMessage());
+      ok = false;
+
+    } finally {
+      IOUtils.closeQuietly(in);
+    }
+
+    // Write as XML
+    xml.writeXML(buffer.toString());
+
+    return ok;
+  }
+
+  /**
    * Parse the response as text.
    * 
    * @param connection The HTTP URL connection.
@@ -405,44 +429,5 @@ public final class PSConnector {
   private static void error(XMLWriter xml, String error, String message) throws IOException {
     xml.attribute("error", error);
     xml.attribute("message", message);
-  }
-
-  /**
-   * Returns the fragment part of the URL.
-   * 
-   * @param resource the path to the resource 
-   * @return the part before any '#' or '?'. 
-   */
-  private static String getURLPath(String resource) {
-    int h = resource.lastIndexOf('#');
-    String r = h > 0? resource.substring(0, h) : resource;
-    int q = r.indexOf('?');
-    if (q > 0) return r.substring(0, q);
-    else return r;
-  }
-
-  /**
-   * Returns the query part of the URL.
-   * 
-   * @param resource the path to the resource 
-   * @return the part after and including '?' if it exists; otherwise <code>null</code> 
-   */
-  private static String getURLQuery(String resource) {
-    int q = resource.indexOf('?');
-    int h = resource.lastIndexOf('#');
-    if (q < 0 || (h > 0 && h < q)) return null;
-    if (h > q) return resource.substring(q, h);
-    else return resource.substring(q);
-  }
-
-  /**
-   * Returns the fragment part of the URL.
-   * 
-   * @param resource the path to the resource 
-   * @return the part after and including '#' if it exists; otherwise <code>null</code> 
-   */
-  private static String getURLFragment(String resource) {
-    int h = resource.indexOf('#');
-    return h >= 0 ? resource.substring(h) : null;
   }
 }
---
+++ src/com/weborganic/bastille/pageseeder/PSResource.java
@@ -0,0 +1,306 @@
+package com.weborganic.bastille.pageseeder;
+
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Map.Entry;
+
+import org.weborganic.berlioz.GlobalSettings;
+
+import com.weborganic.bastille.security.ps.PageSeederUser;
+
+/**
+ * Defines a resource to retrieve from PageSeeder.
+ * 
+ * @author Christophe Lauret
+ * @version 13 April 2011
+ */
+public final class PSResource {
+
+  /**
+   * The type of resource accessed.
+   */
+  private final PSResourceType _type;
+
+  /**
+   * The name of the resource to access.
+   */
+  private final String _name;
+
+  /**
+   * The parameters to send.
+   */
+  private final Map<String, String> _parameters;
+
+  /**
+   * Creates a new connection to the specified resource.
+   * 
+   * @param type The type of resource.
+   * @param name The name of the resource to access (depends on the type of resource) 
+   */
+  public PSResource(PSResourceType type, String name) {
+    this._type = type;
+    this._name = name;
+    this._parameters = Collections.emptyMap();
+  }
+
+  /**
+   * Creates a new connection to the specified resource.
+   * 
+   * @param type       The type of resource.
+   * @param name       The name of the resource to access (depends on the type of resource)
+   * @param parameters The parameters to access the resource.
+   */
+  private PSResource(PSResourceType type, String name, Map<String, String> parameters) {
+    this._type = type;
+    this._name = name;
+    this._parameters = parameters;
+  }
+
+  // Getters
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * @return The type of resource requested. 
+   */
+  public PSResourceType type() {
+    return this._type;
+  }
+
+  /**
+   * Returns the name of the resource to access.
+   * 
+   * <dl>
+   *   <dt>servlet</dt>
+   *   <dd>The full class name of the servlet (may include parameters)</dd>
+   *   <dt>service</dt>
+   *   <dd>The path name of the servlet (may include parameters)</dd>
+   *   <dt>resource</dt>
+   *   <dd>The full path of the resource (may include parameters)</dd>
+   * </dl>
+   * 
+   * @return the name of the resource to access.
+   */
+  public String name() {
+    return this._name;
+  }
+
+  /**
+   * Add a parameter to this request.
+   * 
+   * @param name  The name of the parameter
+   * @param value The value of the parameter
+   */
+  public void addParameter(String name, String value) {
+    this._parameters.put(name, value);
+  }
+
+  // Private helpers
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * Returns the URL to access this resource.
+   * 
+   * <p>If the user is specified, its details will be included in the URL so that the resource can
+   * be accessed on his behalf.
+   * 
+   * @param user A PageSeeder to access this resource.
+   * 
+   * @return the URL to access this resource.
+   * 
+   * @throws MalformedURLException If the URL is not well-formed
+   */
+  public URL toURL(PageSeederUser user) throws MalformedURLException {
+    Properties pageseeder = GlobalSettings.getNode("bastille.pageseeder");
+
+    // Start building the URL
+    StringBuffer url = new StringBuffer();
+    url.append(pageseeder.getProperty("scheme", "http")).append("://");
+    url.append(pageseeder.getProperty("host",   "localhost")).append(":");
+    url.append(pageseeder.getProperty("port",   "8080"));
+
+    // Decompose the resource (in case it contains a query or fragment part)
+    String path  = getURLPath(this._name);
+    String query = getURLQuery(this._name);
+    String frag  = getURLFragment(this._name);
+
+    // Servlets
+    if (this._type == PSResourceType.SERVLET) {
+      url.append(pageseeder.getProperty("servletprefix", "/ps/servlet")).append('/');
+      url.append(path);
+
+    // Services
+    } else if (this._type == PSResourceType.SERVICE) {
+      url.append(pageseeder.getProperty("siteprefix", "/ps"));
+      url.append("/service");
+      url.append(path);
+
+    // Any other resource
+    } else {
+      url.append(path);
+    }
+    // If the session ID is available
+    if (user != null && user.getJSessionId() != null) {
+      url.append(";jsessionid=").append(user.getJSessionId());
+    }
+    // Query Part
+    if (query != null) {
+      url.append(query);
+      url.append("&xformat=xml");
+    } else {
+      url.append("?xformat=xml");
+    }
+    try {
+      for (Entry<String, String> p : this._parameters.entrySet()) {
+        URLEncoder.encode(p.getKey(), "utf-8");
+        url.append("&").append(URLEncoder.encode(p.getKey(), "utf-8"));
+        url.append("=").append(URLEncoder.encode(p.getValue(), "utf-8"));
+      }
+    } catch (UnsupportedEncodingException ex) {
+      // Should never happen as UTF-8 is supported
+      ex.printStackTrace();
+    }
+    // Fragment if any
+    if (frag != null)
+      url.append(frag);
+    return new URL(url.toString());
+  }
+
+  // Private helpers
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * Returns the fragment part of the URL.
+   * 
+   * @param resource the path to the resource 
+   * @return the part before any '#' or '?'. 
+   */
+  private static String getURLPath(String resource) {
+    int h = resource.lastIndexOf('#');
+    String r = h > 0? resource.substring(0, h) : resource;
+    int q = r.indexOf('?');
+    if (q > 0) return r.substring(0, q);
+    else return r;
+  }
+
+  /**
+   * Returns the query part of the URL.
+   * 
+   * @param resource the path to the resource 
+   * @return the part after and including '?' if it exists; otherwise <code>null</code> 
+   */
+  private static String getURLQuery(String resource) {
+    int q = resource.indexOf('?');
+    int h = resource.lastIndexOf('#');
+    if (q < 0 || (h > 0 && h < q)) return null;
+    if (h > q) return resource.substring(q, h);
+    else return resource.substring(q);
+  }
+
+  /**
+   * Returns the fragment part of the URL.
+   * 
+   * @param resource the path to the resource 
+   * @return the part after and including '#' if it exists; otherwise <code>null</code> 
+   */
+  private static String getURLFragment(String resource) {
+    int h = resource.indexOf('#');
+    return h >= 0 ? resource.substring(h) : null;
+  }
+
+  // Private helpers
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * A builder for this resource.
+   * 
+   * @author Christophe Lauret
+   * @version 14 April 2011
+   */
+  public static final class Builder {
+
+    /** The type of resource accessed. */
+    private PSResourceType _type;
+
+    /**
+     * The name of the resource to access.
+     */
+    private String _name;
+
+    /**
+     * The parameters to send.
+     */
+    private Map<String, String> _parameters = new HashMap<String, String>();
+
+    /**
+     * Creates a new builder for a PageSeeder resource.
+     */
+    public Builder() {
+    }
+
+    /**
+     * Creates a new builder for a PageSeeder resource.
+     * 
+     * @param type The type of resource.
+     * @param name The name of the resource to access (depends on the type of resource) 
+     */
+    public Builder(PSResourceType type, String name) {
+      this._type = type;
+      this._name = name;
+    }
+
+    /**
+     * Sets the type of the resource.
+     * @param type the type of the resource to retrieve.
+     * @return this builder
+     */
+    public Builder type(PSResourceType type) {
+      this._type = type;
+      return this;
+    }
+
+    /**
+     * Sets the name of the resource.
+     * @param name the name of the resource to retrieve.
+     * @return this builder
+     */
+    public Builder name(String name) {
+      this._name = name;
+      return this;
+    }
+
+    /**
+     * Add a parameter to this request.
+     * 
+     * @param name  The name of the parameter
+     * @param value The value of the parameter
+     * @return this builder.
+     */
+    public Builder addParameter(String name, String value) {
+      this._parameters.put(name, value);
+      return this;
+    }
+
+    /**
+     * Build the resource from the specified arguments.
+     * @return The corresponding resource.
+     */
+    public PSResource build() {
+      if (this._type == null) throw new IllegalStateException("Unable to build PSResource, type is not set.");
+      if (this._name == null) throw new IllegalStateException("Unable to build PSResource, name is not set.");
+      if (this._parameters.isEmpty()) {
+        return new PSResource(this._type, this._name);
+      } else {
+        // Make parameters immutable
+        return new PSResource(this._type, this._name, new HashMap<String, String>(this._parameters));
+      }
+    }
+
+  }
+
+}

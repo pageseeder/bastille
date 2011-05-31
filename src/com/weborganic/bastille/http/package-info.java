0c0e7ff55e93d71aaae99cbd381c993183595fa5
clauret <devnull@localhost>
2011-05-31 18:03:57 +1000

    Added HTTP packages

---
+++ src/com/weborganic/bastille/http/package-info.java
@@ -0,0 +1,7 @@
+package com.weborganic.bastille.http;
+
+/**
+ * This package provides a collection of useful content generators for use by Berlioz and for HTTP.
+ * 
+ * <p>See individual content generator implementation for details.
+ */

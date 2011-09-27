c9e48d08e4dc5de127e4ae8fd0838f158a6460f0
clauret <devnull@localhost>
2011-09-27 17:43:00 +1000

    New generator to clear index

---
+++ src/com/weborganic/bastille/flint/ClearIndex.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2011 weborganic systems pty. ltd.
+ */
+package com.weborganic.bastille.flint;
+
+import java.io.IOException;
+
+import org.weborganic.berlioz.content.Environment;
+import org.weborganic.berlioz.BerliozException;
+import org.weborganic.berlioz.Beta;
+import org.weborganic.berlioz.content.ContentGenerator;
+import org.weborganic.berlioz.content.ContentRequest;
+
+import com.topologi.diffx.xml.XMLWriter;
+import com.weborganic.bastille.flint.helpers.IndexMaster;
+import com.weborganic.bastille.flint.helpers.MultipleIndex;
+import com.weborganic.bastille.flint.helpers.SingleIndex;
+
+/**
+ * Delete the index.
+ * 
+ * <p>The index must be located in the '/index' directory. 
+ * 
+ * <p>Note: access to this is generator MUST be made secured in the Web descriptor.
+ * 
+ * @author Christophe Lauret 
+ * @version 0.6.20 - 26 September 2011
+ * @since 0.6.20
+ */
+@Beta
+public final class ClearIndex implements ContentGenerator  {
+
+  @Override
+  public void process(ContentRequest req, XMLWriter xml) throws BerliozException, IOException {
+
+    Environment env = req.getEnvironment();
+    String index = req.getParameter("index");
+
+    if (index == null) {
+      // Single index
+      IndexMaster single = SingleIndex.master();
+      if (single == null) {
+        single = SingleIndex.setupMaster(env.getPrivateFile("ixml/default.xsl"));
+      }
+      single.clear();
+      xml.openElement("index");
+      xml.attribute("status", "clear");
+      xml.closeElement();
+
+    } else {
+
+      // Multiple indexes
+      String[] names = index.split(",");
+      xml.openElement("indexes");
+      for (String name : names) {
+        IndexMaster master = MultipleIndex.getMaster(env.getPrivateFile("index/"+name));
+        master.clear();
+        xml.openElement("index");
+        xml.attribute("name", name);
+        xml.attribute("status", "clear");
+        xml.closeElement();
+      }
+      xml.closeElement();
+    }
+  }
+
+}

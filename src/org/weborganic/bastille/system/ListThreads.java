be7314d6fb9c93dba72e6105f3e9164c2d931b70
clauret <devnull@localhost>
2013-02-04 18:40:31 +1100

    Fix null pointer when CPU times are requested

--- src/org/weborganic/bastille/system/ListThreads.java
+++ src/org/weborganic/bastille/system/ListThreads.java
@@ -8,6 +8,8 @@
 package org.weborganic.bastille.system;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -40,18 +42,24 @@ public class ListThreads implements ContentGenerator {
   public void process(ContentRequest req, XMLWriter xml) throws BerliozException, IOException {
 
     boolean stacktraces = "true".equals(req.getParameter("stacktraces"));
+    boolean threadtime = "true".equals(req.getParameter("threadtime"));
+
+    ThreadMXBean bean = threadtime? ManagementFactory.getThreadMXBean() : null;
+    if (bean != null && !bean.isThreadCpuTimeSupported()) bean = null;
 
     xml.openElement("threads");
 
     if (stacktraces) {
       // Use slow but convenient method to load the threads with their stack traces
       Map<Thread, StackTraceElement[]> all = Thread.getAllStackTraces();
-      toXML(all, xml);
+      for (Entry<Thread, StackTraceElement[]> e : all.entrySet()) {
+        toXML(e.getKey(), e.getValue(), bean, xml);
+      }
 
     } else {
       // Use old-school method
       ThreadGroup root = getRootThreadGroup();
-      toXML(root, xml);
+      toXML(root, bean, xml);
     }
 
     xml.closeElement();
@@ -75,11 +83,13 @@ public class ListThreads implements ContentGenerator {
    * Display the specified group thread.
    *
    * @param group The group thread to display as a tree
+   * @param bean  The management bean for CPU time (may be <code>null</code>)
    * @param xml   The XML Writer
    *
    * @throws IOException Should an error occur while writing the XML
    */
-  private static void toXML(ThreadGroup group, XMLWriter xml) throws IOException {
+  private static void toXML(ThreadGroup group, ThreadMXBean bean, XMLWriter xml) throws IOException {
+
     // Grab all the threads
     Thread[] threads = new Thread[group.activeCount()];
     while (group.enumerate(threads, true) == threads.length) {
@@ -98,6 +108,17 @@ public class ListThreads implements ContentGenerator {
         xml.attribute("alive", Boolean.toString(t.isAlive()));
         xml.attribute("daemon", Boolean.toString(t.isDaemon()));
         xml.attribute("group", t.getThreadGroup().getName());
+
+        if (bean != null) {
+          final long cpu = bean.getThreadCpuTime(t.getId());
+          final long user = bean.getThreadUserTime(t.getId());
+          xml.openElement("times");
+          xml.attribute("cpu", Long.toString(cpu));
+          xml.attribute("user", Long.toString(user));
+          xml.attribute("system", Long.toString(cpu - user));
+          xml.closeElement();
+        }
+
         xml.closeElement();
       }
     }
@@ -106,38 +127,53 @@ public class ListThreads implements ContentGenerator {
   /**
    * Return all the threads with stack traces
    *
-   * @param all The threads
+   * @param thread     The thread to serialise as XML
+   * @param stacktrace The stack trace (may be <code>null</code>)
+   * @param bean       The management bean for CPU time (may be <code>null</code>)
    * @param xml The XML writer
+   *
    * @throws IOException If thrown while writing XML.
    */
-  private static void toXML(Map<Thread, StackTraceElement[]> all, XMLWriter xml) throws IOException {
-    for (Entry<Thread, StackTraceElement[]> e : all.entrySet()) {
-      Thread t = e.getKey();
-      StackTraceElement[] s = e.getValue();
-      xml.openElement("thread", true);
-      xml.attribute("id", Long.toString(t.getId()));
-      xml.attribute("name", t.getName());
-      xml.attribute("priority", t.getPriority());
-      xml.attribute("state", t.getState().name());
-      xml.attribute("alive", Boolean.toString(t.isAlive()));
-      xml.attribute("daemon", Boolean.toString(t.isDaemon()));
-      xml.attribute("group", t.getThreadGroup().getName());
-      if (s != null) {
-        xml.openElement("stacktrace");
-        for (StackTraceElement ste : s) {
-          xml.openElement("element");
-          String method = ste.getMethodName();
-          String filename = ste.getFileName();
-          int line = ste.getLineNumber();
-          xml.attribute("class", ste.getClassName());
-          if (filename != null) xml.attribute("filename", filename);
-          if (method != null) xml.attribute("method", method);
-          if (line >= 0) xml.attribute("line", line);
-          xml.closeElement();
-        }
+  private static void toXML(Thread thread, StackTraceElement[] stacktrace, ThreadMXBean bean, XMLWriter xml)
+      throws IOException {
+    xml.openElement("thread", true);
+    xml.attribute("id", Long.toString(thread.getId()));
+    xml.attribute("name", thread.getName());
+    xml.attribute("priority", thread.getPriority());
+    xml.attribute("state", thread.getState().name());
+    xml.attribute("alive", Boolean.toString(thread.isAlive()));
+    xml.attribute("daemon", Boolean.toString(thread.isDaemon()));
+    xml.attribute("group", thread.getThreadGroup().getName());
+
+    // If the management bean is available include times
+    if (bean != null) {
+      final long cpu = bean.getThreadCpuTime(thread.getId());
+      final long user = bean.getThreadUserTime(thread.getId());
+      xml.openElement("time");
+      xml.attribute("cpu", Long.toString(cpu));
+      xml.attribute("user", Long.toString(user));
+      xml.attribute("system", Long.toString(cpu - user));
+      xml.closeElement();
+    }
+
+    // If the stack trace is enabled
+    if (stacktrace != null) {
+      xml.openElement("stacktrace");
+      for (StackTraceElement element : stacktrace) {
+        xml.openElement("element");
+        String method = element.getMethodName();
+        String filename = element.getFileName();
+        int line = element.getLineNumber();
+        xml.attribute("class", element.getClassName());
+        if (filename != null) xml.attribute("filename", filename);
+        if (method != null) xml.attribute("method", method);
+        if (line >= 0) xml.attribute("line", line);
         xml.closeElement();
       }
       xml.closeElement();
     }
+
+    xml.closeElement();
   }
+
 }

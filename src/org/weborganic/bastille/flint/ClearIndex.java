4aa3156a1052a919eb22c37ce02530486b66f7a1
clauret <devnull@localhost>
2013-02-25 19:11:49 +1100

    Check index names

--- src/org/weborganic/bastille/flint/ClearIndex.java
+++ src/org/weborganic/bastille/flint/ClearIndex.java
@@ -7,10 +7,17 @@
  */
 package org.weborganic.bastille.flint;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.weborganic.bastille.flint.config.FlintConfig;
 import org.weborganic.bastille.flint.helpers.IndexMaster;
+import org.weborganic.bastille.flint.helpers.IndexNames;
+import org.weborganic.bastille.util.Errors;
 import org.weborganic.berlioz.BerliozException;
 import org.weborganic.berlioz.Beta;
 import org.weborganic.berlioz.content.ContentGenerator;
@@ -21,23 +28,38 @@ import com.topologi.diffx.xml.XMLWriter;
 /**
  * Clears the content of the index.
  *
- * <p>The index must be located in the '/index' directory.
+ * <p>The index(es) must be located in the directory specified by the <code>FlintConfig</code>.
+ *
+ * <p>Multiple indexes can be cleared at once if specified as a comma separated list.
  *
  * <p>Note: access to this is generator MUST be made secured in the Web descriptor.
  *
  * @author Christophe Lauret
- * @version 0.7.4 - 18 October 2012
+ * @version 0.8.9 - 25 February 2013
  * @since 0.6.20
  */
 @Beta
 public final class ClearIndex implements ContentGenerator  {
 
+  /** Logger for debugging */
+  private static final Logger LOGGER = LoggerFactory.getLogger(CreateIndex.class);
+
+  /** Name of the index parameter. */
+  private static final String INDEX_PARAMETER = "index";
+
   @Override
   public void process(ContentRequest req, XMLWriter xml) throws BerliozException, IOException {
 
-    String index = req.getParameter("index");
+    String index = req.getParameter(INDEX_PARAMETER);
 
     if (index == null) {
+
+      // Check that we are in single index config.
+      if (FlintConfig.hasMultiple()) {
+        LOGGER.warn("Attempted to clear a single index in multiple index configuration");
+        Errors.noParameter(req, xml, INDEX_PARAMETER);
+        return;
+      }
 
       // Single index
       IndexMaster single = FlintConfig.getMaster();
@@ -48,8 +70,21 @@ public final class ClearIndex implements ContentGenerator  {
 
     } else {
 
-      // Multiple indexes
-      String[] names = index.split(",");
+      // Check that we are in single index config.
+      if (!FlintConfig.hasMultiple()) {
+        LOGGER.warn("Attempted to clear a named index in single index configuration");
+        Errors.invalidParameter(req, xml, INDEX_PARAMETER);
+        return;
+      }
+
+      List<String> names = toIndexNames(index);
+
+      // No valid index names where specified
+      if (names.isEmpty()) {
+        Errors.invalidParameter(req, xml, "index");
+        return;
+      }
+
       xml.openElement("indexes");
       for (String name : names) {
         IndexMaster master = FlintConfig.getMaster(name);
@@ -61,6 +96,33 @@ public final class ClearIndex implements ContentGenerator  {
       }
       xml.closeElement();
     }
+  }
+
+  /**
+   * Return the list of index names from the specified index parameter.
+   *
+   * <p>Only includes names which are valid and corresponding to an existing index.
+   *
+   * @param index The index parameter
+   * @return the corresponding list.
+   */
+  private static List<String> toIndexNames(String index) {
+    List<String> names = new ArrayList<String>();
+
+    // Check the index names
+    for (String name : index.split(",")) {
+      if (IndexNames.isValid(name)) {
+        File f = new File(FlintConfig.directory(), name);
+        if (f.exists() && f.isDirectory()) {
+          names.add(name);
+        } else {
+          LOGGER.debug("Invalid index name '{}' was specified", name);
+        }
+      } else {
+        LOGGER.debug("Invalid index name '{}' was specified", name);
+      }
+    }
+    return names;
   }
 
 }
--- src/org/weborganic/bastille/flint/GetFlintConfig.java
+++ src/org/weborganic/bastille/flint/GetFlintConfig.java
@@ -1,20 +1,42 @@
+/*
+ * This file is part of the Bastille library.
+ *
+ * For licensing information please see the file license.txt included in the release.
+ * A copy of this licence can also be found at
+ *   http://www.opensource.org/licenses/artistic-license-2.0.php
+ */
 package org.weborganic.bastille.flint;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.weborganic.bastille.flint.config.FlintConfig;
 import org.weborganic.bastille.flint.config.IFlintConfig;
 import org.weborganic.berlioz.BerliozException;
 import org.weborganic.berlioz.Beta;
 import org.weborganic.berlioz.content.ContentGenerator;
 import org.weborganic.berlioz.content.ContentRequest;
+import org.weborganic.berlioz.util.ISO8601;
 
 import com.topologi.diffx.xml.XMLWriter;
 
+/**
+ * Return the flint configuration attributes and the list of indexes in the system.
+ *
+ * @author Christophe Lauret
+ * @version 25 February 2013
+ */
 @Beta
 public final class GetFlintConfig implements ContentGenerator {
+
+  /** Logger for debugging */
+  private static final Logger LOGGER = LoggerFactory.getLogger(GetFlintConfig.class);
 
   /**
    * To list only folders
@@ -40,12 +62,41 @@ public final class GetFlintConfig implements ContentGenerator {
       if (directory.exists() && directory.isDirectory()) {
         File[] subdirs = directory.listFiles(FOLDERS_ONLY);
         for (File f : subdirs) {
-          xml.openElement("index");
-          xml.attribute("name", f.getName());
-          xml.closeElement();
+          toBasicIndexXML(xml, f);
         }
       }
     }
+    xml.closeElement();
+  }
+
+  /**
+   * Returns the basic index XML by just checking that the index directory exists and is recognised by Lucene.
+   *
+   * @param xml   The XML Writer to use.
+   * @param index The directory containing the index.
+   *
+   * @throws IOException Should an error occur while writing the XML.
+   */
+  private void toBasicIndexXML(XMLWriter xml, File index) throws IOException {
+    xml.openElement("index");
+    xml.attribute("name", index.getName());
+    Directory dir = null;
+    try {
+      dir = FSDirectory.open(index);
+      boolean exists = IndexReader.indexExists(dir);
+      xml.attribute("exists", Boolean.toString(exists));
+      if (exists) {
+        long modified = IndexReader.lastModified(dir);
+        xml.attribute("modified", ISO8601.format(modified, ISO8601.DATETIME));
+      }
+
+    } catch (IOException ex) {
+      LOGGER.warn("Error while tring to read index {}", index.getName());
+
+    } finally {
+      if (dir != null) dir.close();
+    }
+
     xml.closeElement();
   }
 

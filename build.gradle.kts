bb171a33e09277882f368392da32e24d2655f3eb
clauret <clauret@weborganic.com>
2026-05-24 09:14:29 +1000

    Chore: Replace Nexus Staging with JReleaser configuration and upgrade Gradle to 8.14.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,30 @@
 plugins {
     `java-library`
     `maven-publish`
-    signing
-    alias(libs.plugins.nexus.staging)
+    alias(libs.plugins.jreleaser)
 }
 
 val title: String by project
+val website: String by project
+val gitName: String by project
 
 group       = "org.pageseeder.bastille"
 version     = file("version.txt").readText().trim()
 description = title
 
-apply(from = "gradle/publish-mavencentral.gradle.kts")
-
 java {
+    withJavadocJar()
+    withSourcesJar()
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+}
+
+tasks.named<Javadoc>("javadoc") {
+    (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
 }
 
 repositories {
@@ -50,10 +55,49 @@ tasks.test {
     useJUnitPlatform()
 }
 
-nexusStaging {
-    packageGroup = project.group.toString()
-    stagingProfileId = findProperty("sonatypeStagingProfileId") as String?
-    serverUrl = "https://s01.oss.sonatype.org/service/local/"
-    username = findProperty("sonatypeUsername") as String?
-    password = findProperty("sonatypePassword") as String?
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                name.set(title)
+                description.set(project.description)
+                url.set(website)
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                organization {
+                    name.set("Allette Systems")
+                    url.set("https://www.allette.com.au")
+                }
+                scm {
+                    url.set("git@github.com:pageseeder/$gitName.git")
+                    connection.set("scm:git:git@github.com:pageseeder/$gitName.git")
+                    developerConnection.set("scm:git:git@github.com:pageseeder/$gitName.git")
+                }
+                developers {
+                    developer { name.set("Christophe Lauret"); email.set("clauret@weborganic.com") }
+                    developer { name.set("Jean-Baptiste Reure"); email.set("jbreure@weborganic.com") }
+                    developer { name.set("Carlos Cabral"); email.set("ccabral@allette.com.au") }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+    }
+}
+
+jreleaser {
+    configFile.set(file("jreleaser.toml"))
+}
+
+tasks.wrapper {
+    gradleVersion = "8.14.1"
+    distributionType = Wrapper.DistributionType.ALL
 }
